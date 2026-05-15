c300073c14aa16d807eb17445475419ca7d6afc5
Jando08 <alejandroleyva911@gmail.com>
2026-05-14 17:06:43 -0700

    Aprendi los tipos de operadores y como funcionan

---
+++ 03_operadores_aritmeticos.py
@@ -0,0 +1,21 @@
+#suma y resta (+ y -)
+suma = 5 + 2
+resta = 10 - 3
+
+#multiplicacion y division (* y /)
+multiplicacion = 10 * 2
+division = 15 / 5  #devuelve un dato float
+
+
+#potenciacion (**)
+exponente = 2 ** 3  #2 elevado a la 3
+
+#division baja (//)
+divison_baja = 20 // 6 #devuelve entero redondeado hacia bajo
+
+#resto (%)
+resto = 30 % 7
+
+print(resto)
+
+
