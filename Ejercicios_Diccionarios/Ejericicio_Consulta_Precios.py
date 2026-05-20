e74e9a45163e5db30fad1fa8684bdfb26120dee9
Jando08 <alejandroleyva911@gmail.com>
2026-05-19 22:38:18 -0700

    Ejercicio metodo .get() para diccionario

---
+++ Ejercicios_Diccionarios/Ejericicio_Consulta_Precios.py
@@ -0,0 +1,16 @@
+#crear diccionario
+frutas = {
+    'manzana' : 25,
+    'platano' : 15,
+    'mango' : 30
+}
+
+#pedir al usuario que escriba una fruta
+fruta = input('Por favor ingrese una fruta(Manzana, Platano o Mango): ')
+fruta = fruta.lower()
+
+#obtener el precio de la fruta con el metodo .get(clave a buscar, valor por defecto)
+resultado = frutas.get(fruta, 'No disponible')
+
+#imprimir resultado final
+print(f'El precio de la fruta {fruta}: {resultado}')
