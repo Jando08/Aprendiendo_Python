85f0735dbf0816ffc3200872cf31ec0b528205fb
Jando08 <alejandroleyva911@gmail.com>
2026-05-24 12:38:55 -0700

    Variables y Tipos de Datos

---
+++ Aprendiendo_Python_Roadmap/Variables_Tipos_de_Datos.py
@@ -0,0 +1,9 @@
+# variables y tipos de datos en python
+
+nombre = "Alejandro"
+edad = 20
+precio_bitcoin = 64500.50
+me_gusta_python = True
+
+# imprimir mi nombre y edad
+print(f"Mi nombre es {nombre} y mi edad es {edad}")
