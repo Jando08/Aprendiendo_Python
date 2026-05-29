d487637f3e5939354cbf92e8fd30f800bbb78bb0
Jando08 <alejandroleyva911@gmail.com>
2026-05-29 07:28:53 -0700

    Ejercicio de listas basquetbol

---
+++ Aprendiendo_Python_Roadmap/Bucles_Listas/Monitor_NBA.py
@@ -0,0 +1,15 @@
+"""
+Crea un archivo nuevo dentro de tu carpeta recién creada.
+Crea una lista llamada clasificados : "Celtics", "Warriors", "Lakers", y "Nuggets".
+Usa un ciclo for para recorrer la lista de equipos.
+Dentro del bucle, imprime un mensaje. El resultado en la consola debería verse así:
+Equipo clasificado: Celtics
+Equipo clasificado: Warriors
+y así con los demás...
+"""
+
+clasificados = ['Celtics', 'Warriors', 'Lakers', 'Nuggets']
+
+for equipo in clasificados:
+    print(f'\nEquipo clasificado: {equipo} ')
+
